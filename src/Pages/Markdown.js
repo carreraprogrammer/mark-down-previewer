7089a483ad47968a4d1eeb0a56621136e127bf0b
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-28 21:15:55 -0500

    Using markdown Api

--- src/Pages/Markdown.js
+++ src/Pages/Markdown.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import Editor from '../Components/Editor';
-import Preview from '../Components/Preview';
+import React, { useState } from 'react';
+import detective from '../Images/detective.png';
+import ReactMarkdown from 'react-markdown';
 
-class Markdown extends React.Component {
-    render() {
-      return (
-        <section className="mark-down-container">
-            <Editor />
-            <Preview />
+export default function Markdown (){
+
+    const [input, setInput] = useState();    
+
+    return (
+      <section id="markdown-container">
+          <section id="editor-container">
+            <div id="title-container">
+              <h1 id="title">Put Your Markdown Below!</h1>
+              <img id="detective" alt="detective" src={detective}/>
+            </div>
+            <textarea 
+              autoFocus
+              id="editor" 
+              value={input} 
+              onChange={ (e) => setInput(e.target.value)} 
+            />
+          </section>
+          <ReactMarkdown className='markdown' source={input}/>
         </section>
-      )
-    }
+    )
 }
-
-export default Markdown;
